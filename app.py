13715463888067252e2d070c239c89018711517e
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2023-01-27 15:56:09 +0000

    refactor python app files

--- app.py
+++ app.py
@@ -36,49 +36,8 @@ app.__name__ = 'App'
 
 app.mount("/public", StaticFiles(directory="public"), name="public")
 
-# @app.get('/', response_class=HTMLResponse)
-# def root():
-#     todos = Todo.all()
-#     return render("home", todos=todos)
-#
-# @app.get('/hello', response_class=HTMLResponse)
-# def hello2():
-#     return render("page2")
-
-# @app.post('/todos')
-# def todo_create(item: str = Form(...)):
-#     todo = Todo.create(item)
-#     print(f'Todo created: {todo.__dict__}')
-#     return RedirectResponse("/", status_code=303)
-
-# @app.post('/todos/{todo_id}')
-# def todo_update(todo_id: int, item: str = Form(...)):
-#     todo = Todo.get(todoId)
-#     todo.update_text(item)
-#     print(f'Todo created: {todo.__dict__}')
-#     return RedirectResponse("/", status_code=303)
-
-# @app.post('/todos/{todo_id}/check')
-# def todo_check(todo_id: int):
-#     todo = Todo.get(todo_id)
-#     todo.check()
-#     print(f'Todo {todo.id} checked/unchecked')
-#     # data = jsonify(todo.__dict__)
-#     data = todo.__dict__
-#     return data
-
-# @app.get('/open_ai', response_class=HTMLResponse)
-# def open_ai():
-#     return render("open_ai", outputt=output)
-#
-# @app.post('/open_ai/insert')
-# def todo_update(item: str = Form(...)):
-#     index(connection, item)
-#     return RedirectResponse("/open_ai", status_code=303)
-
 @app.get('/open_ai')
 def open_ai():
-    # return render("open_ai", outputt=output)
     dic = {"output":output[0]}
     return dic
 
--- connection.py
+++
@@ -1,9 +0,0 @@
-# sample connection check
-
-# import sqlite3
-# from sqlite3 import Error
-# from service import create_connection
-
-# db_url = ".../sqlite.db"
-# connection = create_connection(db_url)
-# ...
