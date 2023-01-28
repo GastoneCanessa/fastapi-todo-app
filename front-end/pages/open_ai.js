117690fd8aefe2f5d1c2553d78534f02a55d9651
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2023-01-28 09:38:19 +0000

    rename hello to openai - comment current fetch call to python backend - use fetch to call local api first

--- front-end/pages/open_ai.js
+++ front-end/pages/open_ai.js
@@ -1,6 +1,11 @@
 import { useState, useEffect } from "react";
 import Script from 'next/script'
 
+const fetchHeaderJSON = {
+  'Accept': 'application/json',
+  'Content-Type': 'application/json'
+}
+
 const renderPosts = (data, prompt) => {
   let inputValue = document.getElementById("input").value;
   document.getElementById("input").value = `${inputValue}\n\n# ${prompt}\n\n${data}`
@@ -18,20 +23,37 @@ const insertData = async (prompt, res) => {
 
 const fetchData = async (prompt) => {
   console.log(`fetch - body: ${JSON.stringify(prompt)}`)
+  const payload = { "insert": prompt }
   try {
-    const response = await fetch('http://127.0.0.1:4000/open_ai/insert/', {
+    const response = await fetch('/api/open_ai', {
       method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ "insert": prompt }),
+      headers: fetchHeaderJSON,
+      body: JSON.stringify(payload),
     });
     return await response.json();
   } catch (err) {
     console.error(err);
   }
 }
+
+// TODO: move to the server with `getServerSideProps()`
+
+// const fetchData = async (prompt) => {
+//   console.log(`fetch - body: ${JSON.stringify(prompt)}`)
+//   try {
+//     const response = await fetch('http://127.0.0.1:4000/open_ai/insert/', {
+//       method: 'POST',
+//       headers: {
+//         'Accept': 'application/json',
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify({ "insert": prompt }),
+//     });
+//     return await response.json();
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
 
 export default function () {
 
@@ -50,7 +72,7 @@ export default function () {
         <form className="form">
           <div className="mb-4">
             <label className="textarea_label">Code block:</label>
-            <textarea rows="4" cols="50" type="text" class="prompt_textarea" placeholder="Type something" onChange={(e) => prompt = e.target.value}
+            <textarea rows="4" cols="50" type="text" className="prompt_textarea" placeholder="def your_function(): # ..." onChange={(e) => prompt = e.target.value}
             ></textarea>
           </div>
         </form>
@@ -63,7 +85,7 @@ export default function () {
 
         <div className="ouput">
           <h2>Output</h2>
-          <pre class="outputCode">
+          <pre className="outputCode">
             ...
           </pre>
         </div>
